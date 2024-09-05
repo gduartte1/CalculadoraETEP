a95c7c84a8dd3ae47bc1dd74c98c4f818bee4a3c
gduartte1 <guisoaresduarte@hotmail.com>
2024-09-05 20:37:17 -0300

    Atualização calculadora
    - Adicionado condição para caso o operador não seja válido.
    - Adicionado função de loop para realizar mais cálculos caso o usuário deseje.

--- import operator.py
+++ import operator.py
@@ -1,20 +1,49 @@
 import operator
+def calculate():
+    operation = input('''
+Por favor digite a operação matematica que você deseja realizar:
++ para adição
+- para subtração
+* para multiplicação
+/ para divisão
+''')
 
-dict_operadores = {
-'+': operator.add,
-'-': operator.sub,
-'*': operator.mul,
-'/': operator.truediv,
-}
+    number_1 = float(input('Entre o seu 1º número: '))
+    number_2 = float(input('Entre o seu 2º número: '))
 
-print ("digite um número")
-numero1 = int(input( ))
+    if operation == '+':
+        result = number_1 + number_2
+        print('{} + {} = {}'.format(number_1, number_2,result))
+        
 
-print("digite um operador")
-operador = (input( ))
+    elif operation == '-':
+        result = number_1 - number_2
+        print('{} - {} = {}'.format(number_1, number_2,result))
 
-print("digite um segundo número")
-numero2 = int(input( ))
-resultado = (dict_operadores[operador](numero1,numero2))
+    elif operation == '*':
+        result = number_1 * number_2
+        print('{} * {} = {}'.format(number_1, number_2,result))
 
-print("o resultado é",resultado)
+    elif operation == '/':
+        result = number_1 / number_2
+        print('{} / {} = {}'.format(number_1, number_2,result))
+
+    else:
+        print('Você não digitou um operador válido, por favor rode o programa novamente.')
+
+    again()
+
+def again():
+    calc_again = input('''
+Você deseja realizar um novo cálculo?
+Por favor digite S para SIM e N para NÃO.
+''')
+
+    if calc_again.upper() == 'S':
+        calculate()
+    elif calc_again.upper() == 'N':
+        print('Até mais.')
+    else:
+        again()
+
+calculate()
