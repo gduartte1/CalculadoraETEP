84c81755fa918a5381d5a322a4c63acebcac1a37
gduartte1 <guisoaresduarte@hotmail.com>
2024-09-12 13:40:32 -0300

    Criação da função da conversora de unidade de medida
    Criação da função da conversora de unidade de medida

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
-# Function to perform the calculation
+# Função que realizará o cálculo
 def calculate(operation, number_1, number_2):
     try:
         number_1 = float(number_1)
@@ -19,19 +19,17 @@ def calculate(operation, number_1, number_2):
                 result = number_1 / number_2
             else:
                 return "Erro: Divisão por zero não é permitida."
-        else:
-            return "Erro: Operador inválido."
 
         return result
     except ValueError:
         return "Erro: Entrada inválida. Por favor forneceça números."
-
-# Route to render the calculator page
+    
+#Rota para renderizar a página "calculator"
 @app.route('/')
 def calculator():
     return render_template('calculator.html')
 
-# Route to handle the calculation
+#Rota para manipular o calculo
 @app.route('/calculate', methods=['POST'])
 def handle_calculation():
     data = request.get_json()
@@ -43,6 +41,91 @@ def handle_calculation():
     result = calculate(operation, number_1, number_2)
     
     return jsonify(result=str(result))
+
+# Função que realizará a conversão de unidade de medida
+def unitConversion(unit1, unit2, unitvalue_1):
+    print(f'unit1: {unit1}')
+    print(f'unit2: {unit2}')
+    print(f'unitvalue_1: {unitvalue_1}')
+    
+    try:
+        unitvalue_1 = float(unitvalue_1)
+        
+        # Dictionary to store conversions from 'g' to other units
+        g_conversions = {
+            'kg': unitvalue_1 / 1000,
+            'g': unitvalue_1,
+            'ton': unitvalue_1 / 1000000,
+            'lbs': unitvalue_1 / 453.592,
+            'oz': unitvalue_1 / 28.34952
+        }
+        
+        # Dictionary to store conversions from 'kg' to other units
+        kg_conversions = {
+            'g': unitvalue_1 * 1000,
+            'ton': unitvalue_1 / 1000,
+            'lbs': unitvalue_1 * 2.204623,
+            'oz': unitvalue_1 * 35.27396,
+            'kg': unitvalue_1
+        }
+        
+        # Dictionary to store conversions from 'ton' to other units
+        ton_conversions = {
+            'g': unitvalue_1 * 1000000,
+            'kg': unitvalue_1 * 1000,
+            'lbs': unitvalue_1 * 2204.6,
+            'oz': unitvalue_1 * 35274,
+            'ton': unitvalue_1
+        }
+        
+        # Dictionary to store conversions from 'lbs' to other units
+        lbs_conversions = {
+            'g': unitvalue_1 * 453.592,
+            'ton': unitvalue_1 / 2204.6,
+            'kg': unitvalue_1 / 2.2046,
+            'oz': unitvalue_1 * 16,
+            'lbs': unitvalue_1
+        }
+        
+        # Dictionary to store conversions from 'oz' to other units
+        oz_conversions = {
+            'g': unitvalue_1 * 28.34952,
+            'ton': unitvalue_1 / 35273.96,
+            'kg': unitvalue_1 / 35.27396,
+            'lbs': unitvalue_1 / 16,
+            'oz': unitvalue_1
+        }
+        
+        # Dictionary for unit conversion maps
+        conversion_map = {
+            'g': g_conversions,
+            'kg': kg_conversions,
+            'ton': ton_conversions,
+            'lbs': lbs_conversions,
+            'oz': oz_conversions
+        }
+        
+        # Perform the conversion using the map
+        unitResult = conversion_map.get(unit1, {}).get(unit2, "Erro: Conversão inválida.")
+        
+        return unitResult
+    except ValueError:
+        return "Erro: Entrada inválida. Por favor forneça números."
+    
+
+#Rota para manipular o calculo
+@app.route('/unitConversion', methods=['POST'])
+def handle_conversion():
+    data = request.get_json()
+    
+    unit1 = data.get('unit1')
+    unit2 = data.get('unit2')
+    unitvalue_1 = data.get('unitvalue_1')
+    
+    unitResult = unitConversion(unit1, unit2, unitvalue_1)
+    
+    return jsonify(unitResult=str(unitResult))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -79,3 +79,117 @@ document.getElementById("icon_coin").addEventListener("click", function() {
         calculator.style.display = "none";
     };
 })
+
+function UnitConversion() {
+    const unit1 = document.getElementById('unit1').value;
+    const unit2 = document.getElementById('unit2').value;
+    const unitvalue_1 = document.getElementById('unitvalue_1').value;
+
+    fetch('/unitConversion', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            unit1: unit1,
+            unit2: unit2,
+            unitvalue_1: unitvalue_1,
+        }),
+    })
+    .then(response => response.json())  
+    .then(data => {
+        console.log(data)
+        document.getElementById('unitResult').innerText = `${unitvalue_1}${unit1} é o equivalente à ${data.unitResult}${unit2}`;
+    })
+    .catch(error => {
+        document.getElementById('unitResult').innerText = 'Error: Invalid input';
+    });
+}
+
+document.getElementById('conversionType').addEventListener('change', function() {
+    const conversionType = this.value;
+    const unit1 = document.getElementById('unit1');
+    const unit2 = document.getElementById('unit2');
+    
+    // Clear the existing options in the second select
+    unit1.innerHTML = '';
+    unit2.innerHTML = '';
+    
+    // Define options based on the selected value of the first select
+    let unit1options = [];
+    let unit2options = [];
+    if (conversionType == 'peso') {
+        unit1options = [
+            { value: 'g', text: 'g (grama)' },
+            { value: 'kg', text: 'kg (kilograma)' },
+            { value: 'ton', text: 'ton (tonelada)' },
+            { value: 'lbs', text: 'lbs (Libras)' },
+            { value: 'oz', text: 'oz (Onças)' }
+        ];
+        unit2options = [
+            { value: 'g', text: 'g (grama)' },
+            { value: 'kg', text: 'kg (kilograma)' },
+            { value: 'ton', text: 'ton (tonelada)' },
+            { value: 'lbs', text: 'lbs (Libras)' },
+            { value: 'oz', text: 'oz (Onças)' }
+        ];
+    } else if (conversionType == 'velocidade') {
+        unit1options = [
+            { value: 'carrot', text: 'Sexo Anal' },
+            { value: 'broccoli', text: 'Broccoli' },
+            { value: 'spinach', text: 'Spinach' }
+        ];
+        unit2options = [
+            { value: 'carrot', text: 'Carrot' },
+            { value: 'broccoli', text: 'Broccoli' },
+            { value: 'spinach', text: 'Spinach' }  
+        ];
+    } else if (conversionType == 'volume') {
+        unit1options = [
+            { value: 'carrot', text: 'Carrot' },
+            { value: 'broccoli', text: 'Broccoli' },
+            { value: 'spinach', text: 'Spinach' }
+        ];
+        unit2options = [
+            { value: 'carrot', text: 'Carrot' },
+            { value: 'broccoli', text: 'Broccoli' },
+            { value: 'spinach', text: 'Spinach' }  
+        ];
+    } else if (conversionType == 'area') {
+        unit1options = [
+            { value: 'carrot', text: 'Carrot' },
+            { value: 'broccoli', text: 'Broccoli' },
+            { value: 'spinach', text: 'Spinach' }
+        ];
+        unit2options = [
+            { value: 'carrot', text: 'Carrot' },
+            { value: 'broccoli', text: 'Broccoli' },
+            { value: 'spinach', text: 'Spinach' }  
+        ];
+    } else if (conversionType == 'distancia') {
+        unit1options = [
+            { value: 'carrot', text: 'Carrot' },
+            { value: 'broccoli', text: 'Broccoli' },
+            { value: 'spinach', text: 'Spinach' }
+        ];
+        unit2options = [
+            { value: 'carrot', text: 'Carrot' },
+            { value: 'broccoli', text: 'Broccoli' },
+            { value: 'spinach', text: 'Spinach' }  
+        ];
+    }
+
+    // Populate the second select with new options
+    unit1options.forEach(option  => {
+        const newOption1 = document.createElement('option');
+        newOption1.value = option.value;
+        newOption1.text = option.text;
+        unit1.appendChild(newOption1);
+    });
+    unit2options.forEach(option  => {
+        const newOption2 = document.createElement('option');
+        newOption2.value = option.value;
+        newOption2.text = option.text;
+        unit2.appendChild(newOption2);
+    });
+});
